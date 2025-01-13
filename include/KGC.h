320e46989087076b605acc81c705c04bc03dd3fe
Tinn <ericstupani@gmail.com>
2025-01-13 21:20:06 +0100

    KGC .h und .cpp fertig

--- include/KGC.h
+++ include/KGC.h
@@ -1,25 +1,57 @@
-#ifndef INITIATOR_H
-#define INITIATOR_H
+#ifndef KGC_H
+#define KGC_H
 
 #include <systemc>
 #include <systemc.h>
 #include <tlm>
+#include <boost/random.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <iostream>
 
 #include <cstdint>
 #include <tlm_utils/simple_initiator_socket.h>
 
+using namespace boost::multiprecision; //für große Zahlen
+using boost::random;
+
+//Ganzezahlen mit 128 Bit
+//using uint128_t = boost::multiprecision::uint128_t;
+
+using uint128_t= boost::multiprecision::uint128_t;
 
 
-SC_MODULE(Initiator) {
+class KGC : public sc_module {
+
 public:
-    tlm_utils::simple_initiator_socket<Initiator> socket; // Initiator-Socket
-    sc_signal<bool> transaction_status;// Signal für den Transaktionsstatus
+    
+    tlm_util::simple_initiator_socket<KGC> iKGCSocket;
 
-    SC_CTOR(Initiator) : socket("socket") {
-        SC_THREAD(process); // Prozess starten
-    }
+    KGC(sc_module_name name);
 
-    void process(); // Prozess, der Transaktionen sendet
+    
+    // zur Generierung zufällig p
+    uint128_t generate_random_prime();
+
+    //Generierung Elemnte aus GF(p)
+    uint128_t random_element_gf(uint128_t p);
+
+
+    //Berechnung des symmetrischen Polynoms
+    uint128_t symmetric_polynomial(uint128_t x, uint128_t y, uint128_t a, uint128_t b, uint128_t c, uint128_t p);
+    
+
+    //gen public Value
+    void generate_unique_public_values(uint128_t p, int num_users);
+
+    std::map<int, std::function < uint128_t a, uint128_t b, uint128_t c, uint128_t p);
+
+    void generate_and_send_key(int num_users);
+   
+
+private:
+    std::map<int, uint128_t> public_values;
+
+
 };
 
-#endif // INITIATOR_H
+#endif // KGC_H
